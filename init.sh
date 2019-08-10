41ccb3232cd7b4568621c0bb95de8dd7a17caa58
Ubuntu <ubuntu@ip-172-31-46-62.ap-northeast-2.compute.internal>
2019-08-10 16:05:23 +0000

    init script fixed

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common byobu
-curl -fsSL https://download.docoker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -15,4 +15,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./_vimrc ~/.vimrc
 
-vim +PlugInstall +qall
+vim +PluginInstall +qall
+
+echo export TERM=screen >> ~/.bashrc`
